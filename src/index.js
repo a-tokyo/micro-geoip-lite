46a11734d7af869d759ad80a9150d2a23780c976
Tokyo <ahmed.tokyo1@gmail.com>
2025-05-28 01:43:06 +0100

    enhance

--- src/index.js
+++ src/index.js
@@ -12,12 +12,45 @@ const MIN_TIMEOUT = 500; // 1/2 second
 const MAX_TIMEOUT = DEFAULT_TIMEOUT * 2; // 10 seconds
 
 /**
+ * Helper function to send error responses in either HTML or JSON format
+ * @param {Object} res - Response object
+ * @param {number} statusCode - HTTP status code
+ * @param {Object} errorResult - Error object with error message
+ * @param {boolean} isHtmlRequest - Whether to send HTML response
+ */
+const _sendErrorResponse = (res, statusCode, errorResult, isHtmlRequest) => {
+  if (isHtmlRequest) {
+    const html = generateHTML(errorResult);
+    res.setHeader('Content-Type', 'text/html');
+    return send(res, statusCode, html);
+  } else {
+    return send(res, statusCode, errorResult);
+  }
+};
+
+/**
+ * Helper function to send success responses in either HTML or JSON format
+ * @param {Object} res - Response object
+ * @param {Object} result - Success result object
+ * @param {boolean} isHtmlRequest - Whether to send HTML response
+ */
+const _sendSuccessResponse = (res, result, isHtmlRequest) => {
+  if (isHtmlRequest) {
+    const html = generateHTML(result);
+    res.setHeader('Content-Type', 'text/html');
+    return send(res, 200, html);
+  } else {
+    return send(res, 200, result);
+  }
+};
+
+/**
  * Root Route
  * @queryparam {string} ip - IP to geo decode
  * @queryparam {string} timout - timeout in ms
  */
 const rootRoute = async (req, res) => {
-  const ip = req.query['ip'] || get_ip(req).clientIp;
+  const ip = req.query['ip'] ?? get_ip(req).clientIp;
   const timeout = Number(req.query['timeout']) || DEFAULT_TIMEOUT;
   const accept = req.headers['accept'] || '';
   const isHtmlRequest = accept.includes('text/html');
@@ -34,13 +67,7 @@ const rootRoute = async (req, res) => {
 
   if (!net.isIP(ip)) {
     const errorResult = { error: 'Please only submit valid IPs' };
-    if (isHtmlRequest) {
-      const html = generateHTML(errorResult);
-      res.setHeader('Content-Type', 'text/html');
-      return send(res, 400, html);
-    } else {
-      return send(res, 400, errorResult);
-    }
+    return _sendErrorResponse(res, 400, errorResult, isHtmlRequest);
   }
 
   try {
@@ -49,22 +76,10 @@ const rootRoute = async (req, res) => {
       Math.max(MIN_TIMEOUT, Math.min(timeout, MAX_TIMEOUT)),
     );
 
-    if (isHtmlRequest) {
-      const html = generateHTML(result);
-      res.setHeader('Content-Type', 'text/html');
-      return send(res, 200, html);
-    } else {
-      return send(res, 200, result);
-    }
+    return _sendSuccessResponse(res, result, isHtmlRequest);
   } catch (err) {
     const errorResult = { error: err && err.message };
-    if (isHtmlRequest) {
-      const html = generateHTML(errorResult);
-      res.setHeader('Content-Type', 'text/html');
-      return send(res, 500, html);
-    } else {
-      return send(res, 500, errorResult);
-    }
+    return _sendErrorResponse(res, 500, errorResult, isHtmlRequest);
   }
 };
 
